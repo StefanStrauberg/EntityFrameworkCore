8bac57c2851b0eb5544ae61da79fb8f415469c81
StefanStrauberg <stefanstrauberg@gmail.com>
2023-12-30 22:34:24 +0300

    Add Context into Data Layer and create Console Layer

---
+++ src/EntityFrameworkCore.Data/FootballLeageDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data;
+
+public class FootballLeageDbContext : DbContext
+{
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Coach> Coaches { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost; " + 
+            "Initial Catalog=FootballLeage_EfCore; " +
+            "User ID=sa; " +
+            "Password=!QAZxsw2; " +
+            "Ecrypt=False;");
+    }
+}
